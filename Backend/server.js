c1169dc1e9f0a538bbb69ba958ef9a0ad12bcce1
sahildevli <sahildevli07@gmail.com>
2026-01-22 00:45:03 +0530

    env update

--- Backend/server.js
+++ Backend/server.js
@@ -66,7 +66,9 @@ const server = http.createServer((req, res) => {
   res.end("Route not found");
 });
 
-const PORT = 5001;
+
+const PORT = process.env.PORT || 5001;
+
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
